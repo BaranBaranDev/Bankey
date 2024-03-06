b516bdf97c6d5e4dfa946c3c2e2d0dc43df80780
BaranBaranDev <auniversty@gmail.com>
2024-03-06 10:36:41 +0300

    add Onboarding

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -9,21 +9,42 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-    
+    // MARK: - Properties
     private let stackView = UIStackView()
     private let imageView = UIImageView()
     private let label = UILabel()
     
+    private var heroImageView = ""
+    private var titleText = ""
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
     }
+    
+    // MARK: - İnit
+   
+    init(heroImageView: String, titleText: String) {
+        self.heroImageView = heroImageView
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
 }
+
+// MARK: - Helpers
 
 extension OnboardingViewController {
     private func style() {
-        view.backgroundColor = .systemBackground // ! dont forget
+        view.backgroundColor = .systemBackground 
     
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
@@ -32,7 +53,7 @@ extension OnboardingViewController {
         // Image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageView)
         
         // Label
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -40,12 +61,11 @@ extension OnboardingViewController {
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1923."
+        label.text = titleText
     }
     
     private func layout() {
         stackView.addArrangedSubviews(imageView,label)
-        
         view.addSubview(stackView)
         
         NSLayoutConstraint.activate([
