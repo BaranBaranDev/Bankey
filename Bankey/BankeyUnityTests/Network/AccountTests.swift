0b07ba83a2aad3860399dde2f24de8b9e484a499
BaranBaranDev <auniversty@gmail.com>
2024-03-16 14:05:36 +0300

    feat : Add network and unit test

---
+++ Bankey/BankeyUnityTests/Network/AccountTests.swift
@@ -0,0 +1,83 @@
+//
+//  AccountTests.swift
+//  BankeyUnityTests
+//
+//  Created by Baran Baran on 16.03.2024.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankey
+
+class ProfileTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testCanParse() throws {
+        let json = """
+        {
+        "id": "1",
+        "first_name": "Kevin",
+        "last_name": "Flynn",
+        }
+        """
+        
+        let data = json.data(using: .utf8)!
+        let result = try! JSONDecoder().decode(Profile.self, from: data)
+        
+        XCTAssertEqual(result.id, "1")
+        XCTAssertEqual(result.firstName, "Kevin")
+        XCTAssertEqual(result.lastName, "Flynn")
+    }
+}
+
+
+
+@testable import Bankey
+
+class AccountTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    func testCanParse() throws {
+        let json = """
+         [
+           {
+             "id": "1",
+             "type": "Banking",
+             "name": "Basic Savings",
+             "amount": 929466.23,
+             "createdDateTime" : "2010-06-21T15:29:32Z"
+           },
+           {
+             "id": "2",
+             "type": "Banking",
+             "name": "No-Fee All-In Chequing",
+             "amount": 17562.44,
+             "createdDateTime" : "2011-06-21T15:29:32Z"
+           },
+          ]
+        """
+
+
+        let data = json.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        
+        let result = try decoder.decode([Account].self, from: data)
+        
+        XCTAssertEqual(result.count, 2)
+        
+        let account1 = result[0]
+        XCTAssertEqual(account1.id, "1")
+        XCTAssertEqual(account1.type, .Banking)
+        XCTAssertEqual(account1.name, "Basic Savings")
+        XCTAssertEqual(account1.amount, 929466.23)
+        XCTAssertEqual(account1.createdDateTime.monthDayYearString, "Jun 21, 2010")
+    }
+}
